e82104be7db9561859431d1e6fe96769cba6dc80
mdselimme <mdselimdev@gmail.com>
2024-11-30 15:59:19 +0600

    add-json-and-fetch-data

--- src/Components/Shared/AuthProvider/AuthProvider.jsx
+++ src/Components/Shared/AuthProvider/AuthProvider.jsx
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
   const [userData, setUserData] = useState(0);
+  const [residentSingleData, setResidentSingleData] = useState([]);
+
+  // Resident Data Fetch
+  useEffect(() => {
+    fetch("/resident_data.json")
+      .then((res) => res.json())
+      .then((data) => setResidentSingleData(data));
+  }, []);
 
   const authDataAll = {
     userData,
+    residentSingleData,
   };
 
   return (
