00cc79e1bde11662fc84cf6d15923454481d5620
mdselimme <mdselimdev@gmail.com>
2025-01-18 20:46:59 +0600

    add-show-cart-product-delete-item

--- src/Components/Pages/ShowCartProduct/ShowCartProduct.jsx
+++ src/Components/Pages/ShowCartProduct/ShowCartProduct.jsx
@@ -1,8 +1,42 @@
+import Swal from "sweetalert2";
+import axiosSecure from "../../Shared/axiosSecure/axiosSecure";
 import useCartProduct from "../../Shared/useCartProduct/useCartProduct";
 
 const ShowCartProduct = () => {
   const { cart } = useCartProduct();
-  console.log(cart);
+  const { axiosLinker } = axiosSecure();
+  const { refetch } = useCartProduct();
+
+  const deleteCartProduct = (id, title) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You want to delete ${title}`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosLinker
+          .delete(`carts/product/${id}`)
+          .then((result) => {
+            if (result.data.deletedCount) {
+              refetch();
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          })
+          .catch((error) => {
+            console.log(error.message, error.code);
+          });
+      }
+    });
+  };
+
   return (
     <div>
       <div className="bg-emerald-200 p-5 text-center">
@@ -39,26 +73,25 @@ const ShowCartProduct = () => {
             <tbody>
               {/* row 1 */}
               {cart.map((product, index) => (
-                <>
-                  <tr className="text-center">
-                    <th>{index + 1}</th>
-                    <td>
-                      <img
-                        className="w-24 mx-auto"
-                        src={product?.image}
-                        alt=""
-                      />
-                    </td>
-                    <td>{product?.title}</td>
-                    <td>{product?.status ? product?.status : "No status"}</td>
-                    <td>{product?.price}</td>
-                    <td>
-                      <button className="bg-primary px-3 py-2 font-semibold text-white rounded-lg">
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                </>
+                <tr key={index + 3} className="text-center">
+                  <th>{index + 1}</th>
+                  <td>
+                    <img className="w-24 mx-auto" src={product?.image} alt="" />
+                  </td>
+                  <td>{product?.title}</td>
+                  <td>{product?.status ? product?.status : "No status"}</td>
+                  <td>{product?.price}</td>
+                  <td>
+                    <button
+                      onClick={() =>
+                        deleteCartProduct(product?._id, product?.title)
+                      }
+                      className="bg-primary px-3 py-2 font-semibold text-white rounded-lg"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
               ))}
             </tbody>
           </table>
